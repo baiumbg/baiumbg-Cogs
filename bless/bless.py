1b942d7a28b05628d54d29f4833a373c88e82e4a
Veselin Dobrev <veselin.dobrev@jboxers.com>
2021-09-19 17:18:07 +0300

    b

--- bless/bless.py
+++ bless/bless.py
@@ -8,14 +8,12 @@ class Bless(commands.Cog):
     """Bless MU Online utilities."""
 
     def __init__(self, bot):
-        @bot.event
-        async def on_ready():
-            self.bot.loop.create_task(self.watch_auctions())
-
         default_user_config = {'watchlist': []}
         self._config = Config.get_conf(self, identifier=134621854878007301)
         self._config.register_user(**default_user_config)
         self._bot = bot
+
+        self._bot.loop.create_task(self.watch_auctions())
 
     async def watch_auctions():
         while True:
